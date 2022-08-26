acd217771e31e471cafa14a2237c525fb30ba4eb
mamad <mohtashamimohamad99@gmail.com>
2022-08-26 18:17:19 +0300

    show validation errors

--- vue/src/view/Register.vue
+++ vue/src/view/Register.vue
@@ -12,7 +12,19 @@
         </p>
       </div>
       <form class="mt-8 space-y-6" @submit="register">
-        <input type="hidden" name="remember" value="true" />
+      <Alert v-if="Object.keys(errors).length" class="flex-col items-stretch text-sm">
+        <span @click="errors = {}" class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </span>
+        <div v-for="(field, i) of Object.keys(errors)" :key="i">
+          <div v-for="(error, index) of errors[field] || []" :key="index">
+            * {{ error }}
+          </div>
+        </div>
+        
+      </Alert>
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
             <label for="Fullname" class="sr-only">Fullname</label>
@@ -41,10 +53,22 @@
         </div>
 
         <div>
-          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button
+          :disabled="loading"
+          type="submit" 
+          class="group relative w-full flex justify-center py-2 px-4 
+          border border-transparent text-sm font-medium rounded-md text-white 
+          bg-indigo-600 hover:bg-indigo-700 focus:outline-none 
+          focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          :class="{'cursor-not-allowed': loading, 'hover:bg-indigo-500': loading}">
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
               <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
             </span>
+            <svg v-if="loading"
+             class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
             Sign up
           </button>
         </div>
@@ -56,6 +80,8 @@
 import { LockClosedIcon } from '@heroicons/vue/solid'
 import store from '../store';
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+import Alert from '../components/Alert.vue';
 
 const router = useRouter();
 const user = {
@@ -65,15 +91,29 @@ const user = {
   password_confirmation: ''
 };
 
+const loading = ref(false);
+
+const errors = ref({});
+
 function register(ev){
   ev.preventDefault();
+
+  loading.value = true;
+
   store
     .dispatch('register', user)
     .then((res)=>{
+      loading.value = false;
       router.push({
         name: 'Dashboard'
       })
-    });
+    })
+    .catch((err)=>{
+      loading.value = false;
+      if(err.response.status === 422) {
+        errors.value = err.response.data.errors
+      }
+    })
 }
 
 
